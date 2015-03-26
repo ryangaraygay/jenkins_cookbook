cfec2ec453affc6b4938d0d652a0501886d0bad9
ryan-a1 <ryan.garaygay@agilone.com>
2015-03-26 01:40:52 -0700

    removed commented out code

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,21 +1,15 @@
 
-# Cookbook Name:: dev2cb
+# Cookbook Name:: jenkins_cookbook 
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 include_recipe 'apt'
-#include_recipe 'java'
 include_recipe 'git'
 include_recipe 'jenkins::java'
 include_recipe 'jenkins::master'
 
-#package 'apache2'
 package 'curl'
 package 'tree'
-
-#service 'apache2' do
-#  action [:enable, :start]
-#end
 
 service "jenkins" do
   supports [:stop, :start, :restart]
@@ -42,14 +36,3 @@ end
 jenkins_job 'TestJob1' do
   config xml
 end
-
-# would have wanted to do java install from webupd8 because Oracle keeps
-# changing their download page and so java cookbook does not work at times
-# but instead decided to use openjdk for now
-#apt_repository "webupd8-java" do
-#  uri "http://ppa.launchpad.net/webupd8team/java/ubuntu"
-#  distribution "precise"
-#  components ["main"]
-#  keyserver "keyserver.ubuntu.com"
-#  key "EEA14886"
-#end
